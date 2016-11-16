0836eb881bb5da110b6c4d85fb054fdd366d5e52
Максим <malahof.maxim@yandex.ru>
2016-11-16 20:23:45 +0400

    Last version

--- Dropp/js/mobile-menu.js
+++ Dropp/js/mobile-menu.js
@@ -1,7 +1,17 @@
+/*открытие меню при нажатии на гамбургер*/
 $('.menu__hamburger').click(function(){
 	if ( $(this).parent().hasClass( "menu_active" ) ) { 
 		$(this).parent().removeClass("menu_active")
 	} else {
 		$(this).parent().addClass("menu_active")
 	}
+});
+
+/*закрытие меню при нажатии на ссылку*/
+$('.menu__link').click(function(){
+ if ($(".menu").hasClass( "menu_active")) { 
+  $(".menu").removeClass("menu_active");
+ } else {
+  $(".menu").addClass("menu_active");
+ }
 });
